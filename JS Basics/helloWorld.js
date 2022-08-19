9fa695918ed54e8afe77780be6ec795a343f3303
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-19 22:22:14 +0530

    html update

--- JS Basics/helloWorld.js
+++
@@ -1,5 +0,0 @@
-// console.log() - Is used to print, what inside the ()
-// we can also use '' and `` instead of ""
-// we use `` in string templates
-// ; is not mandatory but its good for practice
-console.log("Hello world");
