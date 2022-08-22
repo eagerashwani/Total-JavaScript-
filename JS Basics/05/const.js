fa1eb5b1efe06f2be25e940c161a9973edaa18b7
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-22 13:34:39 +0530

    about const

---
+++ JS Basics/05/const.js
@@ -0,0 +1,10 @@
+// declare constant
+
+const carWheels = 4;
+carWheels = 5; // this line gives error
+// redeclaration not allowed
+// reassign not allowed
+console.log(carWheels);
+
+// Good Practice
+// Always says like, I declare a constant carWheels instead of I declare a variable carWheels
