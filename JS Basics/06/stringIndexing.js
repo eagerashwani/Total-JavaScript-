3a12278023fdde326072e8d0498412055a8e2b85
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-22 14:03:12 +0530

    stringIndexing

---
+++ JS Basics/06/stringIndexing.js
@@ -0,0 +1,17 @@
+// string indexing
+
+let firstName = "Ashwani";
+
+// A s h w a n i
+// 0 1 2 3 4 5 6 - Index
+
+console.log(firstName[4]);
+// 4th index have 'a'
+
+
+console.log(firstName.length); // o/p 7
+// length - no of characters including spaces
+
+
+//if you don't know the length, and want to print the 2nd last number
+console.log(firstName[firstName.length - 2]);
