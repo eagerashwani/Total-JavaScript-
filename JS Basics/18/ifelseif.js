388bdc90937b7356b2f7c338c78d56693d1242f8
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-26 06:25:37 +0530

    if else if

---
+++ JS Basics/18/ifelseif.js
@@ -0,0 +1,22 @@
+// suppose you have to categroize your employee based on experience they have
+
+
+let experience = 1;
+
+if(experience < 1){
+    console.log("fresher");
+}
+else if(experience < 2){
+    console.log("SDE");
+}
+else if(experience < 3){
+    console.log("SDE2");
+}
+else if(experience < 4){
+    console.log("Lead Software Engineer");
+}
+else{
+    console.log("Too much experience");
+}
+
+// JS engine stop execution when condition is matched
