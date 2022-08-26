b9ae5b796bd137f734bdf01b15f972465a8f30f6
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-26 07:18:27 +0530

    dowhile

---
+++ JS Basics/23/dowhileloop.js
@@ -0,0 +1,17 @@
+// In simple while loop, we print till the condition satisfied
+// if condition is false, it do nothing
+
+// but in do while loop, it print atleast once
+
+let i = 0;
+
+do{
+    console.log(i);
+    i++;
+}while(i<=10)
+
+console.log("value of i ", i);
+
+// if I change i = 20, it still print 20
+// and value of i is 21
+// at 21, it start matching condition
