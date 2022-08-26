d9da8f2bf11e0725fb379e02b0a62b23d3dc8066
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-26 06:37:27 +0530

    switch

---
+++ JS Basics/19/switch.js
@@ -0,0 +1,51 @@
+// suppose you have to make a condition on the basis of month number, and print month name
+
+// one way is use if elseif
+
+// other way is switch
+
+let monthNumber = 12;
+
+switch(monthNumber){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("Sept");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;
+    default:
+        console.log("Invalid month");
+}
+
+// if you not use than JS engine prints continuosly
+// break statement is used to terminated the flow.
