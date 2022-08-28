0bb15389e2e80d8d8b588cc9d43dbbf6edd93185
Ashwani Kumar <eagerashwani@gmail.com>
2022-08-28 22:43:19 +0530

    iterateObject

--- JS Basics/36/objIterate.js
+++ JS Basics/36/objIterate.js
@@ -19,6 +19,16 @@ const person = {
 for(let key in person){
    //console.log(key);     // name, age, student, movies, my salary
    // console.log(person[key]);   // values dega
+   // console.log(person.key); // undefined x3
    // console.log(key, " : " ,person[key]);  // key value pair
     console.log(`${key} : ${person[key]}`);  // key value pair
+}
+
+
+// 2nd way - Object.keys
+
+console.log(Object.keys(person));  // op - ['name', 'age', 'student', 'movies', 'my salary']
+
+for(let key of Object.keys(person)){
+    console.log(key, " : " , person[key]);
 }
