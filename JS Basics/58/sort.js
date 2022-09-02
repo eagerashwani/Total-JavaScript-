07d885a7962e15c6ec8d491dec979377635de24c
Ashwani Kumar <eagerashwani@gmail.com>
2022-09-02 07:11:08 +0530

    map

---
+++ JS Basics/58/sort.js
@@ -0,0 +1,7 @@
+// sort -  sorts values as strings/sorts an array alphabetically
+
+// it changes the original array
+
+const numbers = [32,644,2367,1200,5000];
+numbers.sort();
+console.log(numbers); 
