9d75383922d772f0ff74ec4e5812fc5c78237bb9
Ashwani Kumar <eagerashwani@gmail.com>
2022-09-02 07:20:41 +0530

    filter

---
+++ JS Basics/56/filter.js
@@ -0,0 +1,8 @@
+// filter
+// filter takes a callback
+// filter's callback always returns a boolean value
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+console.log(evenNumbers);
