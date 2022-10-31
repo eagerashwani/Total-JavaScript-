ccf902b69e47278d8e8122e24b377154046ef2bb
Ashwani Kumar <eagerashwani@gmail.com>
2022-10-31 13:18:02 +0530

    .

---
+++ JS DOM/6_changeElementStyles.js
@@ -0,0 +1,9 @@
+// change the style of the element
+
+// `My Heading` is in Black color
+const heading = document.querySelector('#heading');
+heading.style.color = 'blue';
+
+// properties that has - in it like background-color have some issues
+heading.style.backgroundColor = 'black'
+// instead of - we use Capital letter of second word without space(CamelCase)
